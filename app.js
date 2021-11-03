09973f99d93ad4c30cabce92a28499ac499482e8
Isaac López <isaaclovi@gmail.com>
2021-11-02 22:27:26 -0600

    Probando exprees en google

---
+++ app.js
@@ -0,0 +1,12 @@
+// exports.topsecret = (req, res) => {
+//     let message = req.query.message || req.body.message || 'Repositorio Hola Top Secret!';
+//     res.status(200).send(message);
+//   };
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(3000);
--- index.js
+++
@@ -1,5 +0,0 @@
-exports.topsecret = (req, res) => {
-    let message = req.query.message || req.body.message || 'Repositorio Hola Top Secret!';
-    res.status(200).send(message);
-  };
-  
