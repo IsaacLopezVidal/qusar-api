08e24caad40bd600517e7d08c2192339d3750d89
Isaac López <isaaclovi@gmail.com>
2021-11-04 18:22:23 -0600

    provando variables de entorno en function cloud

--- app.js
+++ app.js
@@ -6,32 +6,38 @@ let { pool  , createPoolAndEnsureSchema} = require('./config/db');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-// app.use(async (req, res, next) => {
-//    if (pool) {
-//      return next();
-//    }
-//    try {
-//      pool = await createPoolAndEnsureSchema();
-//      next();
-//    } catch (err) {
-//    //   logger.error(err);
-//       console.error(err)
-//      return next(err);
-//    }
-//  });
+app.use(async (req, res, next) => {
+   if (pool) {
+     return next();
+   }
+   try {
+     pool = await createPoolAndEnsureSchema();
+     next();
+   } catch (err) {
+   //   logger.error(err);
+      console.error(err)
+     return next(err);
+   }
+ });
 app.get('/', async function(req, res){
    // logger.info("Entro a principal");
    try{
-      console.log(process.env.DB_HOST)
-      // pool = pool || (await createPoolAndEnsureSchema());
-      // const Query =await pool.query(
-      //    'SELECT resques_id,request,ip,time_cast FROM top_secret ORDER BY time_cast DESC LIMIT 5'
-      //  );
-      //  var string=JSON.stringify(Query);
-      //  var json =  JSON.parse(string);
-       res.send(process.env,200);
+      // console.log(process.env.DB_HOST)
+      pool = pool || (await createPoolAndEnsureSchema());
+      const Query =await pool.query(
+         'SELECT resques_id,request,ip,time_cast FROM top_secret ORDER BY time_cast DESC LIMIT 5'
+       );
+       var string=JSON.stringify(Query);
+       var json =  JSON.parse(string);
+       res.send({
+          variables:process.env,
+          data:json
+       },200);
    }catch(e){
       console.log(e)
+      res.send({
+         variables:process.env,
+      },500);
    }
 });
 exports.topsecret=app;
