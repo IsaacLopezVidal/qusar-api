442719fb3a66891668629844a8620657f1037a08
Isaac López <isaaclovi@gmail.com>
2021-11-04 18:08:34 -0600

    provando variables de entorno

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ let { pool  , createPoolAndEnsureSchema} = require('./config/db');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(async (req, res, next) => {
-   if (pool) {
-     return next();
-   }
-   try {
-     pool = await createPoolAndEnsureSchema();
-     next();
-   } catch (err) {
-   //   logger.error(err);
-      console.error(err)
-     return next(err);
-   }
- });
+// app.use(async (req, res, next) => {
+//    if (pool) {
+//      return next();
+//    }
+//    try {
+//      pool = await createPoolAndEnsureSchema();
+//      next();
+//    } catch (err) {
+//    //   logger.error(err);
+//       console.error(err)
+//      return next(err);
+//    }
+//  });
 app.get('/', async function(req, res){
    // logger.info("Entro a principal");
    try{
-      // console.log(req)
-      pool = pool || (await createPoolAndEnsureSchema());
-      const Query =await pool.query(
-         'SELECT resques_id,request,ip,time_cast FROM top_secret ORDER BY time_cast DESC LIMIT 5'
-       );
-       var string=JSON.stringify(Query);
-       var json =  JSON.parse(string);
-       res.send(json,200);
+      console.log(process.env.DB_HOST)
+      // pool = pool || (await createPoolAndEnsureSchema());
+      // const Query =await pool.query(
+      //    'SELECT resques_id,request,ip,time_cast FROM top_secret ORDER BY time_cast DESC LIMIT 5'
+      //  );
+      //  var string=JSON.stringify(Query);
+      //  var json =  JSON.parse(string);
+       res.send(process.env,200);
    }catch(e){
       console.log(e)
    }
--- config/db.js
+++ config/db.js
@@ -1,45 +1,43 @@
 const mysql = require('promise-mysql');
 const fs = require('fs');
-var path = require("path");
-const {parsed} = require('dotenv').config({ path: path.resolve("./")+'/.env' })
 
 let pool;
 const createTcpPoolSslCerts = async config => {
-    const dbSocketAddr = parsed.DB_HOST.split(':');
+    const dbSocketAddr = process.env.DB_HOST.split(':');
     return mysql.createPool({
-      user: parsed.DB_USER, // e.g. 'my-db-user'
-      password: parsed.DB_PASS, // e.g. 'my-db-password'
-      database: parsed.DB_NAME, // e.g. 'my-database'
+      user: process.env.DB_USER, // e.g. 'my-db-user'
+      password: process.env.DB_PASS, // e.g. 'my-db-password'
+      database: process.env.DB_NAME, // e.g. 'my-database'
       host: dbSocketAddr[0], // e.g. '127.0.0.1'
       port: dbSocketAddr[1], // e.g. '3306'
       ssl: {
         sslmode: 'verify-full',
-        ca: fs.readFileSync(parsed.DB_ROOT_CERT), // e.g., '/path/to/my/server-ca.pem'
-        key: fs.readFileSync(parsed.DB_KEY), // e.g. '/path/to/my/client-key.pem'
-        cert: fs.readFileSync(parsed.DB_CERT), // e.g. '/path/to/my/client-cert.pem'
+        ca: fs.readFileSync(process.env.DB_ROOT_CERT), // e.g., '/path/to/my/server-ca.pem'
+        key: fs.readFileSync(process.env.DB_KEY), // e.g. '/path/to/my/client-key.pem'
+        cert: fs.readFileSync(process.env.DB_CERT), // e.g. '/path/to/my/client-cert.pem'
       },
       ...config,
     });
   };
   
   const createTcpPool = async config => {
-    const dbSocketAddr = parsed.DB_HOST.split(':');
+    const dbSocketAddr = process.env.DB_HOST.split(':');
     return mysql.createPool({
-      user: parsed.DB_USER, 
-      password: parsed.DB_PASS, 
-      database: parsed.DB_NAME, 
+      user: process.env.DB_USER, 
+      password: process.env.DB_PASS, 
+      database: process.env.DB_NAME, 
       host: dbSocketAddr[0], 
       port: dbSocketAddr[1], 
       ...config,
     });
   };
   const createUnixSocketPool = async config => {
-    const dbSocketPath = parsed.DB_SOCKET_PATH || '/cloudsql';
+    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
     return mysql.createPool({
-      user: parsed.DB_USER, 
-      password: parsed.DB_PASS, 
-      database: parsed.DB_NAME, 
-      socketPath: `${dbSocketPath}/${parsed.CLOUD_SQL_CONNECTION_NAME}`,
+      user: process.env.DB_USER, 
+      password: process.env.DB_PASS, 
+      database: process.env.DB_NAME, 
+      socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
       ...config,
     });
   };
@@ -53,8 +51,8 @@ const createTcpPoolSslCerts = async config => {
       waitForConnections: true,
       queueLimit: 0, 
     };
-    if (parsed.DB_HOST) {
-      if (parsed.DB_ROOT_CERT) {
+    if (process.env.DB_HOST) {
+      if (process.env.DB_ROOT_CERT) {
         return createTcpPoolSslCerts(config);
       } else {
         return createTcpPool(config);
