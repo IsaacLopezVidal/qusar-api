46b751852ce5b6d4010f4a19816621878ddbfdc1
Isaac López <isaaclovi@gmail.com>
2021-11-08 12:58:03 -0600

    Quito logs

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const { logger } = require("./config/logger");
+// const { logger } = require("./config/logger");
 const  routes  = require("./routes");
 const errors=require('./middleware/errors')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(logger);
+// app.use(logger);
 routes.forEach(({ path, route, middleware }) => {
   if (middleware) {
     app.use(path, middleware, route);
