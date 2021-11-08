5a4e1dd998399f0502e2be4266853b8d2f77a6a5
Isaac Lopez <isaaclovi@gmail.com>
2021-11-07 23:14:51 -0600

    Create tables

--- routes/topsecretRoute.js
+++ routes/topsecretRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router =express.Router();
-const topsecretMiddleware = require('../middleware/topSecret/topsecretMiddleware')
 const validationSchema = require('../middleware/topSecret/validationSchema')
 const topSecretError = require('../error/topSecretError')
 const Satellites = require('../models/Satelites')
 const GetMessage = require('../services/message')
-const MessageError = require('../error/messageError')
+
+const Nave = require('../models/Nave');
+const Posicion = require('../models/Posicion');
 
 function helperCatch(callback){
     return async (req,res,next)=>{
@@ -26,8 +27,10 @@ router.post('/',validationSchema,helperCatch(async(req,res)=>{
     const {satellites} = req.body
     const {satellites:_satellites} = new Satellites(satellites);
     const messages=[]
+    const _nave= new Nave()
     _satellites.forEach(e=>messages.push(e.message))
-    GetMessage(messages)
+    _nave.message = GetMessage(messages)
+    // _nave.position = new Posicion()
     return res.status(200).json({data:req.body})
 }));
 
